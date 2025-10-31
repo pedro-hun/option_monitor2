fc850ce6cfd61e48c00b4e6fdb555fd8510f7e4e
pedro-hun <pedro.hun@novafutura.com.br>
2025-10-31 19:35:30 -0300

    refactor: add type hints to functions in black scholes module

--- src/core/entities/black_scholes.py
+++ src/core/entities/black_scholes.py
@@ -10,7 +10,7 @@ def w(iv: float, tte_years: float) -> float:
     """
     return iv * iv * tte_years
 
-def d1(w, k):
+def d1(w: float, k: float) -> float:
     """
     Auxiliary function to compute d1 from BSM model.
     @param par: Set of raw parameters, (a, b, rho, m, sigma)
@@ -18,11 +18,14 @@ def d1(w, k):
     @param k: Moneyness points to evaluate
     @return: Values of d1 evaluated at k points
     """
+    # if w == 0:
+    #     print(w)
+    #     return np.array([])
     v = np.sqrt(w)
     return -k/v + 0.5*v
 
 
-def d2(w, k):
+def d2(w: float, k: float) -> float:
     """
     Auxiliary function to compute d2 from BSM model.
     @param k: Moneyness points to evaluate
@@ -39,7 +42,7 @@ def discount_factor(r: float, tte_years: float) -> float:
     """
     return (1 + r) ** (-tte_years)
 
-def black_scholes(spot_price: float, strike: float, tte_years: float, iv: float, r: float, option_type: str, k: float, borrow: float) -> float:
+def black_scholes(spot_price: float, strike: float, tte_years: float, iv: float | float, r: float, option_type: str, k: float, borrow: float) -> float:
     """
     Calculate Black-Scholes price from forward price and implied volatility
     """
@@ -51,4 +54,16 @@ def black_scholes(spot_price: float, strike: float, tte_years: float, iv: float,
         price = strike * DF * norm.cdf(-d2(w_val, k)) - spot_price * np.exp(-borrow * tte_years) * norm.cdf(-d1(w_val, k))
     else:
         raise ValueError(f"Invalid option_type: {option_type}. Must be 'call' or 'put'")
-    return float(price)
+    return price
+
+if __name__ == "__main__":
+    import pandas as pd
+
+    def soma (a: float, b: float) -> float:
+        return a + b
+
+    a = pd.Series([1.0, 2.0, 3.0])
+    b = pd.Series([4.0, 5.0, 6.0])
+    df = pd.DataFrame({'bid': a, 'ask': b})
+    soma_series = df.apply(lambda row: soma(row['bid'], row['ask']), axis=1)
+    print(soma_series)
