fd9cb61dbe51cf53347d5769443347acb1745a60
pedro-hun <pedro.hun@novafutura.com.br>
2025-11-04 16:41:24 -0300

    add greek calculation, and apply bs and greek calc

--- src/orchestrators/df_creator.py
+++ src/orchestrators/df_creator.py
@@ -19,11 +19,12 @@ from src.data_fetchers.excel_reader import ExcelReader, ExcelReaderConfig
 from src.data_fetchers.holiday_df_reader import Holidays
 from src.filters.option_filters import option_data_positive, intrinsic_lower_than_price, option_lower_than_underlying, valid_price, valid_obj_func, valid_iv
 from src.utils.option_data_calc import get_type, calculate_tte_days, calculate_tte_years, calculate_mid_price, calculate_bid_ask_spread, calculate_relative_spread, calculate_forward_price, calculate_moneyness, calculate_log_moneyness, calculate_intrinsic_value
-from src.core.entities.black_scholes import black_scholes
+from src.core.entities.black_scholes import black_scholes, w, d1, d2, discount_factor
+from src.core.greeks.greeks_calculation import vega, theta, rho, delta, gamma
 
 
 holidays = Holidays()
-excel_reader_config = ExcelReaderConfig(excel_file="C:/Users/pedro.hun/Documents/repos/option_monitor2/examples/data/petr_options_chain_3.xlsx", sheet_name="Sheet1", iv_col="I", start_row=2, end_row=1925, bid_col="E", ask_col="F", last_price_col="B", strike_col="C", open_interest_col="H", volume_col="D", ticker_col="A", maturity_col="G", spot_price_col="J")
+excel_reader_config = ExcelReaderConfig(excel_file="C:/Users/pedro.hun/Documents/repos/option_monitor2/examples/data/petr_options_chain_test.xlsx", sheet_name="Sheet1", iv_col="I", start_row=2, end_row=557, bid_col="E", ask_col="F", last_price_col="B", strike_col="C", open_interest_col="H", volume_col="D", ticker_col="A", maturity_col="G", spot_price_col="J")
 
 
 
@@ -62,6 +63,19 @@ class BasicFilters:
 
         return filtered_df
     
+class GreeksCalc:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def calculate_greeks(self) -> pd.DataFrame:
+        # Placeholder for greeks calculation logic
+        self.df["Vega"] = self.df.apply(lambda row: vega(spot_price=row["SpotPrice"], tte_years=row["TTE_years"], d1=row["D1"], borrow=0), axis=1)
+        self.df["Theta"] = self.df.apply(lambda row: theta(spot_price=row["SpotPrice"], strike=row["Strike"], tte_years=row["TTE_years"], sigma=row["CalcIV"], d1=row["D1"], d2=row["D2"], r=0.15, option_type=row["OptionType"], borrow=0), axis=1)
+        self.df["Rho"] = self.df.apply(lambda row: rho(strike=row["Strike"], tte_years=row["TTE_years"], d2=row["D2"], r=0.15, option_type=row["OptionType"]), axis=1)
+        self.df["Delta"] = self.df.apply(lambda row: delta(tte_years=row["TTE_years"], d1=row["D1"], option_type=row["OptionType"], borrow=0), axis=1)
+        self.df["Gamma"] = self.df.apply(lambda row: gamma(spot_price=row["SpotPrice"], tte_years=row["TTE_years"], sigma=row["CalcIV"], d1=row["D1"], borrow=0), axis=1)
+        return self.df
+    
 class DFCreator:
     def __init__(self):
         self.dataframe_creator = DataFrameCreator()
@@ -73,23 +87,9 @@ class DFCreator:
         self.filtered_df = basic_filters.apply_filters()
         return self.filtered_df
 
-    def apply_bs(self):
-        self.filtered_df = self.create_filtered_dataframe().copy()
-        self.filtered_df["BSPrice"] = self.filtered_df.apply(lambda row: black_scholes(
-            spot_price=row["SpotPrice"],
-            strike=row["Strike"],
-            tte_years=row["TTE_years"],
-            iv=row["IV"],
-            r=0.15,
-            option_type=row["OptionType"],
-            k=row["LogMoneyness"],
-            borrow=0
-        ), axis=1)
-        return self.filtered_df
-
     
     def apply_iv_calc(self):
-        self.filtered_df = self.apply_bs().copy()
+        self.filtered_df = self.create_filtered_dataframe().copy()
         self.filtered_df["CalcIV"] = self.filtered_df.apply(lambda row: IVCalc(
             market_price=row["MidPrice"],
             forward_price=row["ForwardPrice"],
@@ -103,11 +103,35 @@ class DFCreator:
         ).iv_calculator(), axis=1)
         return self.filtered_df
     
-if __name__ == "__main__":
-    df_creator = DFCreator()
-    final_df = df_creator.apply_iv_calc()
-    print(final_df)
-
-
+    def apply_bs(self):
+        self.filtered_df = self.apply_iv_calc().copy()
+        self.filtered_df["W"] = self.filtered_df.apply(lambda row: w(iv=row["CalcIV"], tte_years=row["TTE_years"]), axis=1)
+        self.filtered_df["D1"] = self.filtered_df.apply(lambda row: d1(w=row["W"], k=row["LogMoneyness"]), axis=1)
+        self.filtered_df["D2"] = self.filtered_df.apply(lambda row: d2(w=row["W"], k=row["LogMoneyness"]), axis=1)
+        self.filtered_df["BSPrice"] = self.filtered_df.apply(lambda row: black_scholes(
+            spot_price=row["SpotPrice"],
+            strike=row["Strike"],
+            tte_years=row["TTE_years"],
+            iv=row["CalcIV"],
+            r=0.15,
+            option_type=row["OptionType"],
+            k=row["LogMoneyness"],
+            borrow=0
+        ), axis=1)
+        return self.filtered_df
     
+    def apply_greeks(self):
+        self.filtered_df = self.apply_bs().copy()
+        greeks_calculator = GreeksCalc(self.filtered_df)
+        self.filtered_df = greeks_calculator.calculate_greeks()
+        return self.filtered_df
+
+
+
+
+
+
+
+
+
 
